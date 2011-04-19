c124b6d235a0c21a6cbd8262b068be782b43cfbf
sam <sam.ibrahiem@aeste.net>
2011-04-19 14:50:02 +0800

    modified the includes of heap_3.c to be used for the project memory allocation

--- Source/portable/GCC/aeMB/main.d
+++
@@ -1,9 +0,0 @@
-main.o: main.c ../../../../Source/include/FreeRTOS.h \
-  /usr/local/mb/bin/../lib/gcc/microblaze-xilinx-elf/4.1.1/include/stddef.h \
-  ../../../../Source/include/../../Source/include/projdefs.h \
-  ../../../../Source/include/../../Source/portable/GCC/aeMB/aeinclude/FreeRTOSConfig.h \
-  ../../../../Source/include/../../Source/include/portable.h \
-  ../../../../Source/include/../../Source/include/../../Source/portable/GCC/aeMB/aeinclude/portmacro.h \
-  ../../../../Source/include/../../Source/include/mpu_wrappers.h \
-  ../../../../Source/include/task.h ../../../../Source/include/portable.h \
-  ../../../../Source/include/list.h
