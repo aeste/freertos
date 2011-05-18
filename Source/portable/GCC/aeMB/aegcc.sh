6347c9e6dac83400480fcd16be8fcb21cbf2194c
sam <sam.ibrahiem@aeste.net>
2011-05-18 12:09:05 +0800

    Replace simboard.hh for the new simulation and change the directory in the script (aegcc.sh) to new simulation directory "aeste"

--- Source/portable/GCC/aeMB/aegcc.sh
+++ Source/portable/GCC/aeMB/aegcc.sh
@@ -10,7 +10,7 @@ make
 mb-objcopy -O srec a.out a.srec 
 
 # Generate a Verilog VMEM file from the SREC file
-srec_cat a.srec -o ../../../../../aemb/sim/dump.vmem -vmem 32
+srec_cat a.srec -o ../../../../../aeste/sim/dump.vmem -vmem 32
 
 # Remove all the object files and srecord files
 rm -f *.o a.srec
