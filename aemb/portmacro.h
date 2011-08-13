efd414748a311f2b292c077c9d6315b2475559b7
Shawn Tan <shawn.tan@aeste.my>
2011-08-14 02:11:14 +0800

    Add license. Minor changes.

--- aemb/portmacro.h
+++ aemb/portmacro.h
@@ -1,55 +1,20 @@
 /*
-    FreeRTOS V7.0.1 - Copyright (C) 2011 Real Time Engineers Ltd.
-	
+ AEMB2 FREERTOS PORT
+ Copyright (C) 2011 AESTE WORKS (M) SDN BHD.
 
-    ***************************************************************************
-     *                                                                       *
-     *    FreeRTOS tutorial books are available in pdf and paperback.        *
-     *    Complete, revised, and edited pdf reference manuals are also       *
-     *    available.                                                         *
-     *                                                                       *
-     *    Purchasing FreeRTOS documentation will not only help you, by       *
-     *    ensuring you get running as quickly as possible and with an        *
-     *    in-depth knowledge of how to use FreeRTOS, it will also help       *
-     *    the FreeRTOS project to continue with its mission of providing     *
-     *    professional grade, cross platform, de facto standard solutions    *
-     *    for microcontrollers - completely free of charge!                  *
-     *                                                                       *
-     *    >>> See http://www.FreeRTOS.org/Documentation for details. <<<     *
-     *                                                                       *
-     *    Thank you for using FreeRTOS, and thank you for your support!      *
-     *                                                                       *
-    ***************************************************************************
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    This file is part of the FreeRTOS distribution.
-
-    FreeRTOS is free software; you can redistribute it and/or modify it under
-    the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation AND MODIFIED BY the FreeRTOS exception.
-    >>>NOTE<<< The modification to the GPL is included to allow you to
-    distribute a combined work that includes FreeRTOS without being obliged to
-    provide the source code for proprietary components outside of the FreeRTOS
-    kernel.  FreeRTOS is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-    or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-    more details. You should have received a copy of the GNU General Public
-    License and the FreeRTOS license exception along with FreeRTOS; if not it
-    can be viewed here: http://www.freertos.org/a00114.html and also obtained
-    by writing to Richard Barry, contact details for whom are available on the
-    FreeRTOS WEB site.
-
-    1 tab == 4 spaces!
-
-    http://www.FreeRTOS.org - Documentation, latest information, license and
-    contact details.
-
-    http://www.SafeRTOS.com - A version that is certified for use in safety
-    critical systems.
-
-    http://www.OpenRTOS.com - Commercial support, development, porting,
-    licensing and training services.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef PORTMACRO_H
 #define PORTMACRO_H
@@ -94,21 +59,19 @@ extern "C" {
 /* Critical section macros. */
 void vPortEnterCritical( void );
 void vPortExitCritical( void );
+//#define portENTER_CRITICAL()	vPortEnterCritical()
+//#define portEXIT_CRITICAL()	vPortExitCritical()
 #define portENTER_CRITICAL()		{														\
 										extern unsigned portBASE_TYPE uxCriticalNesting;	\
 										portDISABLE_INTERRUPTS();					\
 										uxCriticalNesting++;								\
 									}
-									
+
 #define portEXIT_CRITICAL()			{														\
 										extern unsigned portBASE_TYPE uxCriticalNesting;	\
-										/* Interrupts are disabled, so we can */			\
-										/* access the variable directly. */					\
 										uxCriticalNesting--;								\
 										if( uxCriticalNesting == 0 )			\
 										{													\
-											/* The nesting has unwound and we 				\
-											can enable interrupts again. */					\
 											portENABLE_INTERRUPTS();						\
 										}													\
 									}
